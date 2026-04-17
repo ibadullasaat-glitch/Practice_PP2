80527eb1d9be6ef49b6df34ec3d5e080d2d8179d
Ibadulla_Sayat <ibadullasaat@gmail.com>
2026-04-17 11:51:32 +0500

    Add Practice8 - PhoneBook with functions and stored procedures

---
+++ Practice_8/connect.py
@@ -0,0 +1,41 @@
+import psycopg2
+from config import host, port, user, password, db_name
+
+
+try:
+    connection = psycopg2.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        database=db_name
+    )
+    connection.autocommit = True
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT version();")
+        print(f"Server version: {cursor.fetchone()}")
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """CREATE TABLE users(
+            id serial PRIMARY KEY,
+            first_name VARCHAR(50) NOT NULL,
+            nickname VARCHAR(50) NOT NULL);"""
+        )
+
+    print("[INFO]table created successfully")
+
+
+
+
+
+
+
+
+
+except Exception as _ex:
+    print("[INFO] Error while working with PostgreSQL", _ex)
+finally:
+    if connection:
+        connection.close()
+        print("[INFO] PostgreSQL connection closed")
